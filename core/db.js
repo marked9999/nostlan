e7e4132d498ca7bf8ba3e13afd108cd98214a896
Quinton Ashley <qashto@gmail.com>
2021-11-11 17:44:49 -0500

    1.22.1

--- core/db.js
+++ core/db.js
@@ -10,19 +10,19 @@ let sources = {
 	arcade: {
 		win: ['${app}', '--list']
 	},
-	ds: "https://www.gametdb.com/dstdb.txt?LANG=EN",
-	n3ds: "https://www.gametdb.com/3dstdb.txt?LANG=EN",
-	n64: "https://github.com/libretro/libretro-database/raw/master/metadat/no-intro/Nintendo%20-%20Nintendo%2064.dat",
-	nes: "https://github.com/libretro/libretro-database/raw/master/metadat/no-intro/Nintendo%20-%20Super%20Nintendo%20Entertainment%20System.dat",
-	ps1: "http://psxdatacenter.com/ulist.html",
-	ps2: "https://psxdatacenter.com/psx2/ulist2.html",
-	ps3: "https://www.gametdb.com/ps3tdb.txt?LANG=EN",
-	psp: "http://psxdatacenter.com/psp/ulist.html",
-	switch: "https://www.gametdb.com/switchtdb.txt?LANG=EN",
+	ds: 'https://www.gametdb.com/dstdb.txt?LANG=EN',
+	n3ds: 'https://www.gametdb.com/3dstdb.txt?LANG=EN',
+	n64: 'https://github.com/libretro/libretro-database/raw/master/metadat/no-intro/Nintendo%20-%20Nintendo%2064.dat',
+	nes: 'https://github.com/libretro/libretro-database/raw/master/metadat/no-intro/Nintendo%20-%20Super%20Nintendo%20Entertainment%20System.dat',
+	ps1: 'http://psxdatacenter.com/ulist.html',
+	ps2: 'https://psxdatacenter.com/psx2/ulist2.html',
+	ps3: 'https://www.gametdb.com/ps3tdb.txt?LANG=EN',
+	psp: 'http://psxdatacenter.com/psp/ulist.html',
+	switch: 'https://www.gametdb.com/switchtdb.txt?LANG=EN',
 	// "https://switchbrew.org/wiki/Title_list/Games"
-	wii: "https://www.gametdb.com/wiitdb.txt?LANG=EN",
-	wiiu: "https://www.gametdb.com/wiiutdb.txt?LANG=EN",
-	xbox360: "https://www.gamesdatabase.org/xbox_360_games_list_with_title_ids"
+	wii: 'https://www.gametdb.com/wiitdb.txt?LANG=EN',
+	wiiu: 'https://www.gametdb.com/wiiutdb.txt?LANG=EN',
+	xbox360: 'https://www.gamesdatabase.org/xbox_360_games_list_with_title_ids'
 };
 let regionFilter = {
 	wii: {
@@ -30,107 +30,149 @@ let regionFilter = {
 	}
 };
 let format = {
-	arcade: [{
-		regex: /(\S+)\s*("[^\n]+)/gm,
-		replace: `"id": "$1",\n\t"title": $2\n}, {`
-	}],
-	gba: [{
-		regex: /<game name="([^"]+)[^\n]+\n[^\n]+\n[^\n]+\n[^\n]+<crc>([^<]+)[^\n]+\n[^\n]+\n[^\n]+\n[^\n]+\n[^\n]+\n[^\n]+\n[^\n]+\n/gm,
-		replace: `"id": "$2",\n\t"title": "$1"\n}, {\n`
-	}],
-	nes: [{
-		regex: /game \(\s+name "([^\(]+)\(([^\(]+)\)( \([^\d]* *([\d\.]*)\))*[^"]*"[^\n]*\n[^\n]*[^"]*"[^"]*" size \d* crc (\w{8})[^\n]*\n[^\n]*\n\n/gm,
-		replace: `"id": "$5",\n\t\t"title": "$1",\n\t\t"region": "$2",\n\t\t"ver": "$4"\n\t}, {\n\t\t`
-	}],
-	n64: [{
-		regex: /\)\s*game \(/gm,
-		replace: `}, {`
-	}, {
-		regex: /\t*description [^\n]*\n/gm,
-		replace: ``
-	}, {
-		regex: /\tname "/gm,
-		replace: `\t"title": "`
-	}, {
-		regex: /\tregion "/gm,
-		replace: `\t"region": "`
-	}, {
-		regex: /\tserial "/gm,
-		replace: `\t"serial": "`
-	}, {
-		regex: /\tversion "/,
-		replace: `"version": "`
-	}, {
-		regex: /\thomepage "[^"]*"\n/,
-		replace: ``
-	}, {
-		regex: /\t"title": "[^"]*"/,
-		replace: `"sys": "n64"`
-	}, {
-		regex: /clrmamepro \(/,
-		replace: ''
-	}, {
-		regex: /"\n/g,
-		replace: '",\n'
-	}, {
-		regex: /rom \( name "[^"]*" [^ ]* [^ ]* crc ([^ ]*) [^ ]* [^ ]* sha1 ([^ ]*)[^\)]*\)/gm,
-		replace: `"id": "$1",\n\t"sha1": "$2"`
-	}],
-	ps1: [{
-		regex: /\s*"\?[^\n]*(\s*")[^"]*([^\n]*\s*")[^"]*([^,]*),\n[^\n]*\s*\},\s*{/gm,
-		replace: `$1id$2title$3\n\t}, {`
-	}],
-	ps2: [{
-		regex: /\|([^\|]*)\|([^\|]*)\|[^\|]*[^\|\n]*\n/gm,
-		replace: `\t"id": "$1",\n\t"title": "$2"\n}, {\n`
-	}],
-	snes: [{
-		regex: /game\n[^:\n]*: *([^\n]*)\n[^:\n]*: *([^\n]*)\n[^:\n]*: *([^\n]*)\n[^:\n]*: *([^\n]*)\n[^\-\n]*\-[^\-\n]*\-([^\n]*)(\n[^g\n][^\n]*|\n)*/gm,
-		replace: `}, {\n\t"id": "$4-$5",\n\t"title": "$2",\n\t"label": "$3"\n\t"sha256": "$1"\n`
-	}, {
-		regex: /database\n *revision: ([^\n]*)[^\}]*}, /gm,
-		replace: ` {\n "version": "$1", \n "games": [`
-	}, {
-		regex: /\n$/gm,
-		replace: `}]\n}`
-	}],
+	arcade: [
+		{
+			regex: /(\S+)\s*("[^\n]+)/gm,
+			replace: `\n\t"id": "$1",\n\t"title": $2\n}, {`
+		}
+	],
+	gba: [
+		{
+			regex:
+				/<game name="([^"]+)[^\n]+\n[^\n]+\n[^\n]+\n[^\n]+<crc>([^<]+)[^\n]+\n[^\n]+\n[^\n]+\n[^\n]+\n[^\n]+\n[^\n]+\n[^\n]+\n/gm,
+			replace: `\n\t"id": "$2",\n\t"title": "$1"\n}, {`
+		}
+	],
+	nes: [
+		{
+			regex:
+				/game \(\s+name "([^\(]+)\(([^\(]+)\)( \([^\d]* *([\d\.]*)\))*[^"]*"[^\n]*\n[^\n]*[^"]*"[^"]*" size \d* crc (\w{8})[^\n]*\n[^\n]*\n\n/gm,
+			replace: `\n\t"id": "$5",\n\t"title": "$1",\n\t"region": "$2",\n\t"ver": "$4"\n}, {`
+		}
+	],
+	n64: [
+		{
+			regex: /\)\s*game \(/gm,
+			replace: `}, {`
+		},
+		{
+			regex: /\t*description [^\n]*\n/gm,
+			replace: ``
+		},
+		{
+			regex: /\tname "/gm,
+			replace: `\t"title": "`
+		},
+		{
+			regex: /\tregion "/gm,
+			replace: `\t"region": "`
+		},
+		{
+			regex: /\tserial "/gm,
+			replace: `\t"serial": "`
+		},
+		{
+			regex: /\tversion "/,
+			replace: `"version": "`
+		},
+		{
+			regex: /\thomepage "[^"]*"\n/,
+			replace: ``
+		},
+		{
+			regex: /\t"title": "[^"]*"/,
+			replace: `"sys": "n64"`
+		},
+		{
+			regex: /clrmamepro \(/,
+			replace: ''
+		},
+		{
+			regex: /"\n/g,
+			replace: '",\n'
+		},
+		{
+			regex: /rom \( name "[^"]*" [^ ]* [^ ]* crc ([^ ]*) [^ ]* [^ ]* sha1 ([^ ]*)[^\)]*\)/gm,
+			replace: `"id": "$1",\n\t"sha1": "$2"`
+		}
+	],
+	ps1: [
+		{
+			regex: /\s*"\?[^\n]*(\s*")[^"]*([^\n]*\s*")[^"]*([^,]*),\n[^\n]*\s*\},\s*{/gm,
+			replace: `$1id$2title$3\n\t}, {`
+		}
+	],
+	ps2: [
+		{
+			regex: /\|([^\|]*)\|([^\|]*)\|[^\|]*[^\|\n]*\n/gm,
+			replace: `\n\t"id": "$1",\n\t"title": "$2"\n}, {`
+		}
+	],
+	snes: [
+		{
+			regex:
+				/game\n[^:\n]*: *([^\n]*)\n[^:\n]*: *([^\n]*)\n[^:\n]*: *([^\n]*)\n[^:\n]*: *([^\n]*)\n[^\-\n]*\-[^\-\n]*\-([^\n]*)(\n[^g\n][^\n]*|\n)*/gm,
+			replace: `\n\t"id": "$4-$5",\n\t"title": "$2",\n\t"label": "$3"\n\t"sha256": "$1"\n}, {`
+		},
+		{
+			regex: /database\n *revision: ([^\n]*)[^\}]*}, /gm,
+			replace: ` {\n "version": "$1", \n "games": [`
+		},
+		{
+			regex: /\n$/gm,
+			replace: `}]\n}`
+		}
+	],
 	// switch: [{
 	// 	regex: /\n\s*(\{[^\s]*)\s*"[^"]*("[^\n]*\n[^\s]*)\s*"[^"]*([^"]*"[^"]*"[^"]*")([^\}]*\} \w+)*[^\}]*\s+\}/gm,
 	// 	replace: `$1\n\t "id$2\t"title$3\n},`
 	// }],
-	switch: [{
-		regex: /\n([^\n ]*) = ([^\n]*)/gm,
-		replace: `}, {\n\t"id": "$1",\n\t"title": "$2"\n`
-	}, {
-		regex: /[^\n]+\n/,
-		replace: ''
-	}],
-	wii: [{
-		regex: /\n([^\n ]*) = ([^\n]*\n*)/gm,
-		replace: `}, {\n\t"id": "$1",\n\t"title": "$2"\n`
-	}],
-	xbox360: [{
-		regex: /([^\|]*)\|([^\|]*)\|[^\|]*\|[^\|]*\|[^\|\n]*\n/gm,
-		replace: `\t "id": "$1",\n\t "title": "$2"\n}, {\n `
-	}]
+	wii: [
+		{
+			regex: /.*/,
+			replace: ''
+		},
+		{
+			regex: /\n([^\n ]*) = ([^\n]*)/gm,
+			replace: `\n\t"id": "$1",\n\t"title": "$2"\n}, {`
+		}
+	],
+	xbox360: [
+		{
+			regex: /([^\|]*)\|([^\|]*)\|[^\|]*\|[^\|]*\|[^\|\n]*\n/gm,
+			replace: `\n\t "id": "$1",\n\t "title": "$2"\n}, {`
+		}
+	]
 };
 
-let strips = [{
-	regex: /("title": "[^\n"]*)"([^"\n]*)"([^"\n]*)"/gm,
-	replace: `$1\\"$2\\"$3"`
-}, {
-	regex: /(™|©|®|℗)/gm,
-	replace: ``
-}, {
-	regex: / - /gm,
-	replace: `: `
-}, {
-	regex: /&apos;/gm,
-	replace: `'`
-}, {
-	regex: /&amp;/gm,
-	replace: `&`
-}];
+format.ds = format.wii;
+format.n3ds = format.wii;
+format.ps3 = format.wii;
+format.switch = format.wii;
+format.wiiu = format.wii;
+
+let strips = [
+	{
+		regex: /("title": "[^\n"]*)"([^"\n]*)"([^"\n]*)"/gm,
+		replace: `$1\\"$2\\"$3"`
+	},
+	{
+		regex: /(™|©|®|℗)/gm,
+		replace: ``
+	},
+	{
+		regex: / - /gm,
+		replace: `: `
+	},
+	{
+		regex: /&apos;/gm,
+		replace: `'`
+	},
+	{
+		regex: /&amp;/gm,
+		replace: `&`
+	}
+];
 
 class GenDB {
 	constructor() {}
@@ -160,7 +202,7 @@ class GenDB {
 						id: x[1],
 						title: x[2],
 						region: x[3]
-					}
+					};
 					list.push(game);
 				}
 			}
@@ -189,8 +231,7 @@ class GenDB {
 			list = '{' + list;
 			list += '}]}';
 		} else {
-			list = '{\n"games": [{\n' + list;
-			list += '\n}]}';
+			list = '{\n"games": [{' + list.slice(0, -4) + '\n]}';
 		}
 
 		log('saving file: ' + file);
@@ -221,9 +262,7 @@ class GenDB {
 				if (i == 0) continue;
 				let x = list.games[i - sub];
 				let y = list.games[i];
-				if (x.title == y.title &&
-					x.region == y.region &&
-					x.serial == y.serial) {
+				if (x.title == y.title && x.region == y.region && x.serial == y.serial) {
 					x.sha1 = [x.sha1, y.sha1];
 					sub++;
 				} else {
@@ -258,7 +297,7 @@ class GenDB {
 		let merged = 0;
 		for (let game of db0.games) {
 			// merge by id
-			let match = db1.games.find(x => x.id === game.id);
+			let match = db1.games.find((x) => x.id === game.id);
 			if (!match) continue;
 			log(match.title);
 			deepExtend(game, match);
@@ -267,7 +306,7 @@ class GenDB {
 
 		for (let game of db1.games) {
 			// merge by id
-			let match = db0.games.find(x => x.id === game.id);
+			let match = db0.games.find((x) => x.id === game.id);
 			if (!match) {
 				db0.games.push(game);
 				log(game.title);
--- db/cli/db-cli.js
+++ db/cli/db-cli.js
@@ -1,4 +1,4 @@
-module.exports = async function(arg) {
+module.exports = async function (arg) {
 	global.path = require('path');
 	arg.__root = path.join(__dirname, '/../..').replace(/\\/g, '/');
 	await require(arg.__root + '/core/setup.js')(arg);
@@ -7,11 +7,14 @@ module.exports = async function(arg) {
 
 	global.sys = arg.sys;
 
-	if (arg.generate) {
+	if (arg.update) {
+		await db.generate();
+		await db.merge();
+	} else if (arg.generate) {
 		db.generate();
 	} else if (arg.merge) {
 		db.merge();
 	} else {
-		er('cli db can\'t do this action: ' + arg.do);
+		er("cli db can't do this action: " + arg.do);
 	}
-}
+};
